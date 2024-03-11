7f42151671aa246a2c59dba9429c425da271a49f
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2024-03-11 20:19:42 -0300

    test: learning about shared examples

---
+++ exemplo_rspec_tdd/spec/shared_examples/shared_examples_spec.rb
@@ -0,0 +1,15 @@
+require 'person'
+
+shared_examples 'status' do |feeling|
+  it "##{feeling}!" do
+    person.send("#{feeling}!")
+    expect(person.status).to eq("Feeling #{feeling.capitalize}!")
+  end
+end
+
+describe 'Person' do
+  subject(:person){ Person.new }
+
+  include_examples 'status', :happy
+  include_examples 'status', :sad
+end
